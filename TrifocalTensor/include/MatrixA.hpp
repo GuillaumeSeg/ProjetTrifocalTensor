c9195556a6d5556c3a53d4359ee560df741364f3
GuillaumeSeg <guillaume.segado@gmail.com>
2012-12-31 18:26:40 +0100

    Filling MatrixA

---
+++ TrifocalTensor/include/MatrixA.hpp
@@ -0,0 +1,10 @@
+#ifndef MATRIXA__HPP
+#define MATRIXA__HPP
+
+#include "../Eigen/Dense"
+
+void fillingA(Eigen::MatrixXd &A, Eigen::MatrixXd &matrixIMG1, Eigen::MatrixXd &matrixIMG2, Eigen::MatrixXd &matrixIMG3);
+
+#endif
+
+
